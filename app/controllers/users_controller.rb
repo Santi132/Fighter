0892814ea3a7f6e9b4e3225526d385fd902e4d5d
Santi132 <santiago-g95@live.com>
2016-11-04 12:58:48 -0500

    Stuff is happenning

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,24 @@
 class UsersController < ApplicationController
     def new
+      @user = User.new
+    end
+
+    def create
+      @user = User.create(user_params)
+      redirect_to index_path
     end
 
     def index
+      @users = User.all
     end
+
+    def show
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:image, :name, :age, :description)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  resources :users
   root 'static_pages#home'
+  get '/index', to: 'users#index'
+  get '/signup', to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
